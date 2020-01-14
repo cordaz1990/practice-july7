0f4bbb16703085f12e9bf453ef4f571c1e572905
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 18:16:43 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,14 @@
-print("hello")
-print("print)
+#Recursion 
+
+def countdown(n):
+  if n <= 0:
+    print('Blastoff!') 
+  else:
+    print(n)
+    countdown(n-1)
+    
+def print_n(s,n):
+  if n <= 0:
+    return
+  print(s)
+  print(s, n - 1)
